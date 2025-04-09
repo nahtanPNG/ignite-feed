fe343aa09370bb28ddaa0bc7876a34e73cac3a41
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-04-09 11:57:35 -0300

    feat: add post comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ export default function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/nahtanpng.png"/>
+                    <img className={styles.avatar} src="https://github.com/nahtanpng.png" alt="Avatar"/>
                     <div className={styles.authorInfo}>
                         <strong>Nathan Ferreira</strong>
                         <span>FullStack Developer</span>
@@ -17,7 +17,8 @@ export default function Post() {
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da
+                    Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉{' '} <a href="#">jane.design/doctorcare</a></p>
                 <p>
                     <a href="#">#novoprojeto</a>{' '}
@@ -25,6 +26,18 @@ export default function Post() {
                     <a href="#">#rocketseat</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder="Deixe um comentário"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
